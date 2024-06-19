3b22666ade9e082a426bd7fd794e3b28c8e0e440
takkngw <takkngw.08.05@gmail.com>
2024-06-20 00:42:09 +0900

    change concept to math and change design

--- helloworld/models.py
+++ helloworld/models.py
@@ -4,7 +4,7 @@ from django.db import models
 # Create your models here.
 class Helloworld(models.Model):
     title = models.CharField('タイトル', max_length=128)
-    code = models.TextField('コード', blank=True)
+    code = models.TextField('数式', blank=True)
     description = models.TextField('説明', blank=True)
     created_by = models.ForeignKey(settings.AUTH_USER_MODEL,
                                    verbose_name='投稿者',
